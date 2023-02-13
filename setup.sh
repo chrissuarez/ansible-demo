1039899e537211620bf0ea74acdce0909ac22c89
Chris Suarez <chris.suarez.biberle@outlook.com>
2023-02-13 08:00:43 +0000

    updated setup bash script

--- setup.sh
+++ setup.sh
@@ -18,10 +18,18 @@ echo "$username:$password" | sudo chpasswd
 #sudo passwd newuser
 
 # Add the user to the sudo group
-sudo usermod -aG sudo ansible
+#sudo usermod -aG sudo ansible
+
+sudo touch /etc/sudoers.d/ansible
+
+sudo echo 'echo "ansible ALL=(ALL) NOPASSWS:ALL" >> /etc/sudoers.d/ansible'
+
+sudo chown root:root /etc/sudoers.d/ansible
+
+sudo chmod 440 /etc/sudoers.d/ansible
 
 # Edit sudoers file to allow the user to run sudo without password
-sudo bash -c 'echo "ansible ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers'
+#sudo bash -c 'echo "ansible ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers'
 
 # Switch to the new user
 sudo su - ansible
