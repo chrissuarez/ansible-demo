2d62be6b274f5bde8bff4c3e33b55c48288eaff4
Chris Suarez <chris.suarez.biberle@outlook.com>
2023-02-13 07:29:02 +0000

    added setup bash script

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Create a new user
+sudo useradd -m ansible
+
+# Set password for the user
+#sudo passwd newuser
+
+# Add the user to the sudo group
+sudo usermod -aG sudo ansible
+
+# Edit sudoers file to allow the user to run sudo without password
+sudo bash -c 'echo "newuser ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers'
+
+# Switch to the new user
+su - ansible
+
+# Generate SSH key for the user
+ssh-keygen -t rsa
+
+# Share the public key with the server
+ssh-copy-id 192.168.68.103
