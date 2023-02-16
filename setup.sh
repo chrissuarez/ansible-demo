13a4e63adaa8e4dc135e6a1690a6b5fd0664c9cd
Chris Suarez <chris.suarez.biberle@outlook.com>
2023-02-16 07:41:08 +0000

    updated setup file

--- setup.sh
+++ setup.sh
@@ -6,14 +6,14 @@ username=ansible
 # Check if the Ansible user already exists
 if ! id "$username" > /dev/null 2>&1; then
 	# If the user doesn't exist, create it
-	sudo useradd $username
+	sudo useradd -m $username
 fi
 
 # Check if the entry for the Ansible user already exists in the sudoers file
 entry="$username ALL=(ALL) NOPASSWD:ALL"
 if ! sudo grep -q "$entry" /etc/sudoers; then
        	#If it doesn't exist, add the entry
-      	sudo echo "$entry" >> /etc/sudoers
+      	echo "$entry" | sudo tee -a /etc/sudoers > /dev/null
 	echo "Entry added to the sudoers file."
 else
     	# If it does exist, don't add the entry
